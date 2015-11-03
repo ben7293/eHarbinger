3a086318674febcc0332488c2043e98af8c85612
bm1549 <bm1549@nyu.edu>
2015-11-02 19:41:07 -0500

    Return true instead

--- database/insertUser.sql
+++ database/insertUser.sql
@@ -1,14 +1,12 @@
 CREATE OR REPLACE FUNCTION insertUser
 (IN in_username varchar(255),
 IN in_password varchar(255))
-RETURNS varchar(255) AS
+RETURNS boolean AS
 $$
 DECLARE
-	out_username varchar(255) DEFAULT 'Failure';
 BEGIN
 	INSERT INTO users VALUES (in_username, in_password);
-	SELECT username INTO out_username FROM users WHERE username = in_username;
-	RETURN out_username;
+	RETURN true;
 END;
 $$
 LANGUAGE plpgsql;
