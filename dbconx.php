017471f59728f888b79d95690a08c22d08910b5b
Benson Tsai <Benson Tsai>
2015-11-16 15:06:51 -0500

    Edited database attributes for dbconx module for internal testing

--- dbconx.php
+++ dbconx.php
@@ -1,6 +1,6 @@
 <?php
 	function conn_db(){
-		$db = pg_connect("host=localhost dbname=eHarbinger user=root password=admin")
+		$db = pg_connect("host=localhost dbname=bt773 user=bt773 password=")
 			or die("Could not connect to database: " . pg_last_error());
 		return $db;
 	}
