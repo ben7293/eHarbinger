7d4732c816c9cd4f8e80e1eae75dda0150d3a188
Benson Tsai <Benson Tsai>
2015-11-18 18:12:51 -0500

    Testing dbconx module

--- dbconx.php
+++ dbconx.php
@@ -1,6 +1,6 @@
 <?php
 	function conn_db(){
-		$db = pg_connect("host=localhost dbname=bt773 user=bt773 password=")
+		$db = pg_connect("host=localhost dbname=bt773 user=bt773 password=2ijiolgl!!")
 			or die("Could not connect to database: " . pg_last_error());
 		return $db;
 	}
