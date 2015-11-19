779aff403f27853bc895b5f5cfa1bf5feb81f698
Benson Tsai <Benson Tsai>
2015-11-18 22:58:30 -0500

    Test file

--- test.php
+++ test.php
@@ -6,7 +6,11 @@
 include("dbconx.php");
 
 $db = conn_db();
-echo "Success";
+$result = pg_query($db, "SELECT * from users");
+if (!$result){echo "Error!\n";}
+for ($i = 0; $i < pg_num_rows($result; ++$i){
+	echo pg_fetch_row($result);
+}
 
 ?>
 
