90c222dbd5921e5c659b3eaeeac56b787d179369
Benson Tsai <Benson Tsai>
2015-11-18 22:52:09 -0500

    Testing dbconx module

--- dbconx.php
+++ dbconx.php
@@ -1,8 +1,6 @@
 <?php
 	function conn_db(){
-		$db = pg_connect("host=localhost dbname=bt773")
-			or die("Could not connect to database: " . pg_last_error());
-		return $db;
+		return pg_connect("host=localhost dbname=bt773");	
 	}
 	function close_db($db){
 		pg_close($db);
