fd1e14f2ca0be4ed64217a79056534947833e71e
Benson Tsai <bt773@pdc-amd01.poly.edu>
2015-11-22 14:18:29 -0500

    Modified code based on Amit's suggestion

--- dbconx.php
+++ dbconx.php
@@ -7,9 +7,11 @@
 		pg_close($db);
 	}
 echo $db;
-$db = pg_connect("host=localhost dbname=bt773 user=bt773 password=");
+$db = pg_connect("dbname=bt773");
 if (!$db){echo "Empty db\n";}
-$result = pg_query($db, "SELECT * FROM users;");
+echo pg_last_error();
+echo phpversion();
+$result = pg_exec($db, "SELECT * FROM users;");
 $num = pg_num_rows($result);
 echo $num;
 echo "1";
