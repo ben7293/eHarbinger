4e2ad6255e933beeee779f5c0be73321d835bf94
Benson Tsai <Benson Tsai>
2015-12-02 16:05:55 -0500

    Testing dbconx

--- dbconx.php
+++ dbconx.php
@@ -6,7 +6,7 @@
 	function close_db($db){
 		pg_close($db);
 	}
-$db = conn_db();
+$db = pg_connect("dbname=bt773 user=bt773 password=bt773");
 $result = pg_query($db, "SELECT * FROM users;") or die('Query failed: ' . pg_last_error());
 echo $result;
 $fetch = pg_fetch_all($result);
