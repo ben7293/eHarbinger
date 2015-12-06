f9d653c47fb13ad0e54253311db1709119592eea
Benson Tsai <ben7293@gmail.com>
2015-12-06 04:40:48 -0500

    Test

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
         <title>eHarbinger</title>
         <link rel="stylesheet" type="text/css" href="style.css">
         <script type="text/javascript" src = "design.js"></script>
-		<?php include_once("header.php"); ?>
+		<?php 
+			include_once("header.php");
+			session_save_path("/home/FALL2015/bt773/public_html/eHarbinger/sessions");
+			session_start();			
+		?>
 	</head>
 	<body>
 		<header>
