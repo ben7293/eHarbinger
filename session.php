ee3de7f5ef46c2b54af11a749329e6d10f694c5c
Benson Tsai <ben7293@gmail.com>
2015-12-05 23:38:20 -0500

    Test

--- session.php
+++ session.php
@@ -1,16 +1,18 @@
 <?php
 session_start();
 include_once("classes.php");
-if ($_SESSION["user"]->isLoggedIn()){
-	echo "Session verification successful<br />";
-	var_dump(!$_SESSION["user"]->isLoggedIn());
-}
-else{
-	//If there is no session information
-	echo "Session verification failed<br />";
-	var_dump(!$_SESSION["user"]->isLoggedIn());
+var_dump($_SESSION["user"]);
 
-}
+// if ($_SESSION["user"]->isLoggedIn()){
+	// echo "Session verification successful<br />";
+	// var_dump(!$_SESSION["user"]->isLoggedIn());
+// }
+// else{
+	// //If there is no session information
+	// echo "Session verification failed<br />";
+	// var_dump(!$_SESSION["user"]->isLoggedIn());
+
+// }
 
 
 ?>
