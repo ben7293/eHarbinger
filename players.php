e9b25557aacd5df55654d83e1fde4006d19c5d18
Benson Tsai <ben7293@gmail.com>
2015-12-06 03:16:55 -0500

    Fix error

--- players.php
+++ players.php
@@ -10,7 +10,7 @@
 	<body>
 		<header> You Have This Many Matches: (2) </header>
 		<div id = "col-1">
-			<button action="userauth.php?logout=1"> Log Out </button>
+			<button formaction="userauth.php?logout=1"> Log Out </button>
 			<br>
 			<br>
 			<a href = "messages.html" > Messages </a>
