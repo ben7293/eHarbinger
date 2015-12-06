3c31306cbbb4548d1f8c7b0d55a7a611706f1ee4
Benson Tsai <ben7293@gmail.com>
2015-12-06 04:49:57 -0500

    Test

--- players.php
+++ players.php
@@ -9,7 +9,7 @@
 			include_once("header.php");
 			session_start();
 			include_once("session.php");
-						session_start();
+			session_start();
 		?>
 	</head>
 	<body>
--- session.php
+++ session.php
@@ -3,6 +3,7 @@ include_once("dbconx.php");
 include_once("classes.php");
 
 session_start();
+
 if (isset($_SESSION["user"])){
 	if ($_SESSION["user"]->isLoggedIn()){
 		echo "Session verification successful<br />";
