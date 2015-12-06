896556f50adc77cfca6de2876f3beb7b958daffe
Benson Tsai <ben7293@gmail.com>
2015-12-05 22:48:16 -0500

    Test

--- userauth.php
+++ userauth.php
@@ -33,16 +33,13 @@ function userAuth($user, $pxwd){
 <body>
 <?php
 
-userAuth("ben7293", "baby");
-
-// if ($_POST['user'] && $_POST['pxwd']){
-	// userAuth($_POST['user'], $_POST['pxwd']);
-// }
-// else{
-	// echo "Direct access to this page is not allowed.<br />";
-// }
-
-header("Location: session.php");
+if ($_POST['user'] && $_POST['pxwd']){
+	userAuth($_POST['user'], $_POST['pxwd']);
+	header("Location: session.php");
+}
+else{
+	echo "Direct access to this page is not allowed.<br />";
+}
 
 ?>
 </body>
