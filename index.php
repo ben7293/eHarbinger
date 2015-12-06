a622efad71d79f08cbf489783b2d4caaefbe759d
Benson Tsai <ben7293@gmail.com>
2015-12-06 14:42:18 -0500

    Test

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" type="text/css" href="style.css">
         <script type="text/javascript" src = "design.js"></script>
 		<?php 
+			include_once("header.php");
 			session_save_path("/home/FALL2015/bt773/public_html/eHarbinger/sessions");
 			session_start();	
 		?>
