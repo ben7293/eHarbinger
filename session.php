7a2dfd6f6d9bcdd08eb400bd05b246df16400a1d
Benson Tsai <ben7293@gmail.com>
2015-12-06 02:16:55 -0500

    Fixed redirect loop

--- session.php
+++ session.php
@@ -3,15 +3,16 @@ include_once("dbconx.php");
 include_once("classes.php");
 
 session_start();
+if ($_SESSION["user"]{
+	if ($_SESSION["user"]->isLoggedIn()){
+		echo "Session verification successful<br />";
+	}
+	else{
+		//If there is no session information
+		echo "Session verification failed<br />";
+		Header("Location: index.html");
 
-if ($_SESSION["user"]->isLoggedIn()){
-	echo "Session verification successful<br />";
-}
-else{
-	//If there is no session information
-	echo "Session verification failed<br />";
-	Header("Location: index.html");
-
+	}
 }
 
 
