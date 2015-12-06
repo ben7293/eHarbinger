b9ae7a91ceed37822cce42352fec627a8cab41ff
Benson Tsai <ben7293@gmail.com>
2015-12-06 02:21:41 -0500

    Test

--- userauth.php
+++ userauth.php
@@ -19,13 +19,7 @@ function userAuth($user, $pxwd){
 	}
 }
 
-if ($_POST["user"] && $_POST["pxwd"]){
-	userAuth($_POST["user"], $_POST["pxwd"]);
-}
-else{
-	echo "Direct access to this page is not allowed.<br />";
-}
-
+userAuth($_POST["user"], $_POST["pxwd"]);
 
 
 ?>
