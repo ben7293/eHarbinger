0ef9af38c81773927466d2175692d79495c8f78c
Benson Tsai <Benson Tsai>
2015-12-07 17:46:08 -0500

    test

--- session.php
+++ session.php
@@ -10,18 +10,20 @@ if (isset($_SESSION["user"])){
 	if ($_SESSION["user"]->isLoggedIn()){
 		echo $_SERVER["REQUEST_URI"];
 		echo $_SESSION["user"]->isLoggedIn();
+		echo "Session verification successful<br />";
 		if ($_SERVER["REQUEST_URI"] == $index_path){
 			Header("Location: players.php");
+			exit;
 		}
-		echo "Session verification successful<br />";
 	}
 
 }
 else{
 	//If there is no session information
 	echo "Session verification failed<br />";
-	if ($_SERVER["REQUEST_URI"] != "/~bt773/eHarbinger/index.php"){
+	if ($_SERVER["REQUEST_URI"] != $index_path){
 		Header("Location: index.php");
+		exit;
 	}
 
 }
