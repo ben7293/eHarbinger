e7bf40063df869cf0790403e89936423cc001e3b
Benson Tsai <Benson Tsai>
2015-12-07 14:49:50 -0500

    Added header to contactUs

---
+++ contactUs.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8"> 
+        <title>eHarbinger</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script type="text/javascript" src = "design.js"></script>
+		<?php	include_once("session.php"); ?>
+	</head>
+	<body>
+		<header> About Us! </header>
+		<div>
+			<?php include_once("header.php"); ?>
+		</div>
+		<div id = "col-1">
+			
+		</div>
+		<div id = "col-2">
+			<div id = "avatar">
+				<img src = "ProfilePicture.jpg" height = "300px" width = "250px">
+			</div>
+			<div>
+				<h1 class = "heading"> Meghan Clark </h1>
+				<p class = "text"> Meghan Clark is the lead designer for eHarbinger. </p>
+				<p class = "text"> She is currently working towards her B.S in Computer Science at NYU School of Engineering and looks forward to attending graduate school in the Fall. </p>
+				<p class = "text"> She loves playing on Xbox and PC, and enjoys RPG, Strategy, and Simulation games. </p>
+				<p class = "text"> If you wish to contact her, you can email her at mclark414@gmail.com :) </p>
+			</div>
+		</div>
+		<div>
+			<p class = "footer"> <a href = "messages.html" > Messages | </a> <a href = "players.html" > Search For Players | </a> <a href = "contactUs.html"> Meet the Team! </a> </p>
+		</div>
+	</body>
+</html>
