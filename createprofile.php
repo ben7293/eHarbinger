a70efea59c066b1ba38ba1bff7c2b984cd480f7d
Benson Tsai <ben7293@gmail.com>
2015-12-08 01:15:09 -0500

    Test

--- createprofile.php
+++ createprofile.php
@@ -1,33 +1,33 @@
 <?php
 include_once("classes.php");
-// include_once("session.php");
-// session_start();
+include_once("session.php");
+session_start();
 
 //=============================================================
 
 // if (isset($_SESSION["completedPref"])){
 	// if (!$_SESSION["completedPref"]){
 		//Create csv formatted description
-		$prefCsv = "";
+		// $prefCsv = "";
 		
-		if (isset($_POST["Preferences"])){
-			foreach ($_POST["Preferences"] as $pref){
-				if ($prefCsv == ""){
-					// First item
-					$prefCsv = $pref;
-				}
-				else{
-					// Following item
-					$prefCsv = $prefCsv . "," . $pref;
-				}
-			}
-		}
+		// if (isset($_POST["Preferences"])){
+			// foreach ($_POST["Preferences"] as $pref){
+				// if ($prefCsv == ""){
+					// // First item
+					// $prefCsv = $pref;
+				// }
+				// else{
+					// // Following item
+					// $prefCsv = $prefCsv . "," . $pref;
+				// }
+			// }
+		// }
 		
 		// Send profile data to database
-		$username = $_SESSION["user"]->getName();
-		$name = $_POST["name"];
-		$location = $_POST["location"];
-		$lang = $_POST["lang"];
+		// $username = $_SESSION["user"]->getName();
+		// $name = $_POST["name"];
+		// $location = $_POST["location"];
+		// $lang = $_POST["lang"];
 		
 		// $username = "ben7293";
 		// $name = "Benson Tsai";
