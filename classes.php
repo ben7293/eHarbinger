67a2b93810ff4b5c2cd352f37b905caf3f6df10b
Benson Tsai <ben7293@gmail.com>
2015-12-08 01:46:09 -0500

    Test

--- classes.php
+++ classes.php
@@ -68,7 +68,7 @@ class User
 	
 	public function updateProfile($username, $name, $location, $lang, $prefCsv){
 		echo "in updateProfile()";
-		var_dump($conn);
+		var_dump($this->conn);
 		$status = $this->conn->queryTrueFalse(
 			"select updateprofile('$username','$name','$location','$lang','$prefCsv')"
 		);
