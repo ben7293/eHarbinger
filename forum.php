0b908ea94823048da02e8ee4f47ea4a6b8449611
Benson Tsai <ben7293@gmail.com>
2015-12-08 04:14:25 -0500

    Test

--- forum.php
+++ forum.php
@@ -32,7 +32,7 @@
 	<head>
 		<meta charset="utf-8"> 
         <title>eHarbinger</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <!--<link rel="stylesheet" type="text/css" href="style.css">-->
         <script type="text/javascript" src = "design.js"></script>
 		<style>
 		table td{
