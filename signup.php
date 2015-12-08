022bca31287d2f722c79d567e4b15ba3e373acab
Benson Tsai <ben7293@gmail.com>
2015-12-07 22:04:37 -0500

    Test

--- signup.php
+++ signup.php
@@ -32,24 +32,24 @@
 			<label class = "subheading">Systems: </label> <br>
 			<br>
 			<form id="pref" action="createprofile.php" method="post">
-				 <input type="checkbox" name="Preferences[]" value="Xbox">Xbox
-				 <input type="checkbox" name="Preferences[]" value="Playstation">Playstation
-				 <input type="checkbox" name="Preferences[]" value="PC"> PC
-				 <input type="checkbox" name="Preferences[]" value="Moblie"> Moblie<br>
+				 <input type="checkbox" name="Preferences" value="Xbox">Xbox
+				 <input type="checkbox" name="Preferences" value="Playstation">Playstation
+				 <input type="checkbox" name="Preferences" value="PC"> PC
+				 <input type="checkbox" name="Preferences" value="Moblie"> Moblie<br>
 				<br>
 				<label class = "subheading">Games: </label> <br>
 				<br>
-				 <input type="checkbox" name="Preferences[]" value="Racing">Racing
-				 <input type="checkbox" name="Preferences[]" value="MMO">MMO
-				 <input type="checkbox" name="Preferences[]" value="Shooting">Shooting
-				 <input type="checkbox" name="Preferences[]" value="Action">Action<br />
-				 <input type="checkbox" name="Preferences[]" value="Adventure">Adventure
-				 <input type="checkbox" name="Preferences[]" value="RPG">RPG
-				 <input type="checkbox" name="Preferences[]" value="Strategy">Strategy
-				 <input type="checkbox" name="Preferences[]" value="Sports">Sports<br />
-				 <input type="checkbox" name="Preferences[]" value="Casual">Casual
-				 <input type="checkbox" name="Preferences[]" value="Trivia">Trivia
-				 <input type="checkbox" name="Preferences[]" value="Indie">Indie
+				 <input type="checkbox" name="Preferences" value="Racing">Racing
+				 <input type="checkbox" name="Preferences" value="MMO">MMO
+				 <input type="checkbox" name="Preferences" value="Shooting">Shooting
+				 <input type="checkbox" name="Preferences" value="Action">Action<br />
+				 <input type="checkbox" name="Preferences" value="Adventure">Adventure
+				 <input type="checkbox" name="Preferences" value="RPG">RPG
+				 <input type="checkbox" name="Preferences" value="Strategy">Strategy
+				 <input type="checkbox" name="Preferences" value="Sports">Sports<br />
+				 <input type="checkbox" name="Preferences" value="Casual">Casual
+				 <input type="checkbox" name="Preferences" value="Trivia">Trivia
+				 <input type="checkbox" name="Preferences" value="Indie">Indie
 				<br>
 				<br>
 				<button class = "button1" onclick = "newUser()"> Complete Profile </button> 
