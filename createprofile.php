b82ef5937b8584cdc430549791511388296716cb
Benson Tsai <ben7293@gmail.com>
2015-12-08 02:07:15 -0500

    Test

--- createprofile.php
+++ createprofile.php
@@ -25,6 +25,7 @@ if (isset($_SESSION["completedPref"])){
 		
 		//Send profile data to database
 		$username = $_SESSION["user"]->getName();
+		$username = "ben7293";
 		$name = $_POST["name"];
 		$location = $_POST["location"];
 		$lang = $_POST["lang"];
