4926e2f3110f152e9aa07403027046bdce9277c6
Benson Tsai <ben7293@gmail.com>
2015-12-08 01:14:11 -0500

    Test

--- createprofile.php
+++ createprofile.php
@@ -5,41 +5,42 @@ include_once("classes.php");
 
 //=============================================================
 
-// if (isset($_SESSION["completedPref"])){
-	// if (!$_SESSION["completedPref"]){
+if (isset($_SESSION["completedPref"])){
+	if (!$_SESSION["completedPref"]){
 		//Create csv formatted description
 		$prefCsv = "";
 		
-		// if (isset($_POST["Preferences"])){
-			// foreach ($_POST["Preferences"] as $pref){
-				// if ($prefCsv == ""){
-					// // First item
-					// $prefCsv = $pref;
-				// }
-				// else{
-					// // Following item
-					// $prefCsv = $prefCsv . "," . $pref;
-				// }
-			// }
-		// }
+		if (isset($_POST["Preferences"])){
+			foreach ($_POST["Preferences"] as $pref){
+				if ($prefCsv == ""){
+					// First item
+					$prefCsv = $pref;
+				}
+				else{
+					// Following item
+					$prefCsv = $prefCsv . "," . $pref;
+				}
+			}
+		}
 		
 		// Send profile data to database
-		// $username = $_SESSION["user"]->getName();
-		// $name = $_POST["name"];
-		// $location = $_POST["location"];
-		// $lang = $_POST["lang"];
+		$username = $_SESSION["user"]->getName();
+		$name = $_POST["name"];
+		$location = $_POST["location"];
+		$lang = $_POST["lang"];
 		
-		$username = "ben7293";
-		$name = "Benson Tsai";
-		$location = "Taiwan";
-		$lang = "English";		
-		$prefCsv = "A,B,C";
+		// $username = "ben7293";
+		// $name = "Benson Tsai";
+		// $location = "Taiwan";
+		// $lang = "English";		
+		// $prefCsv = "A,B,C";
 		
-		$_SESSION["user"]->conn->queryTrueFalse(
-			"select updateprofile('$username','$name','$location','$lang','$prefCsv')"
-		);
-	// }
-// }
+		var_dump($_SESSION["user"]->conn);
+		// $_SESSION["user"]->conn->queryTrueFalse(
+			// "select updateprofile('$username','$name','$location','$lang','$prefCsv')"
+		// );
+	}
+}
 header("Location: player.php");
 
 	
