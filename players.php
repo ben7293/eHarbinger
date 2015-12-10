a745b7eb76e4a24d123f5f7cbd88f4df1039dc11
Benson Tsai <ben7293@gmail.com>
2015-12-10 02:34:13 -0500

    Test

--- players.php
+++ players.php
@@ -26,8 +26,8 @@
 				$rating = $_SESSION["user"]->query("select getrating('$yourUserName');", "array");
 				echo "<div id = 'avatar'>";
 				echo "</div>";
-					// echo "<img src = 'resource/avatar/$yourUserName.jpg' height='50px'>";
-					echo "<img src = 'resource/avatar/avatar.jpg' height='50px'>";
+					// echo "<img src = 'resource/avatar/$yourUserName' height='50px'>";
+					echo "<img src = 'resource/avatar/avatar height='50px'>";
 				echo "<div>";
 					echo "Username: $yourUserName<br>";
 					echo "Level: Expert<br>";
