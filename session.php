65997213e1df5be64bde39a6ae64d31abe7eac79
Benson Tsai <ben7293@gmail.com>
2015-12-10 02:15:21 -0500

    Test

--- session.php
+++ session.php
@@ -4,7 +4,7 @@ include_once("classes.php");
 $index_path = "eHarbinger/index.php";
 session_start();
 
-		$current_path = explode('/', $_SERVER["REQUEST_URI"])[3];
+		$current_path = explode('/', $_SERVER["REQUEST_URI"]);
 
 if (isset($_SESSION["user"])){
 	if ($_SESSION["user"]->isLoggedIn()){
@@ -12,7 +12,7 @@ if (isset($_SESSION["user"])){
 		// echo $_SESSION["user"]->isLoggedIn();
 		// echo "Session verification successful<br />";
 		// echo $index_path;
-		$current_path = explode('/', $_SERVER["REQUEST_URI"])[3];
+		$current_path = explode('/', $_SERVER["REQUEST_URI"]);
 
 		if ( substr($_SERVER["REQUEST_URI"], -10, 10) == $index_path || substr($_SERVER["REQUEST_URI"], -10, 10) == $root_path){
 			echo "yes";
@@ -26,7 +26,7 @@ else{
 	//If there is no session information
 	// echo "Session verification failed<br />";
 	if (substr($_SERVER["REQUEST_URI"], -10, 10) != $index_path){
-		die(var_dump($current_path));
+		die(var_dump($current_path[3]));
 		Header("Location: index.php");
 		exit;
 	}
