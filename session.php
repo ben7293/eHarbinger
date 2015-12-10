6fb0a578ad568afdc83810df5d67917ae8210ea6
Benson Tsai <ben7293@gmail.com>
2015-12-10 02:01:19 -0500

    Test

--- session.php
+++ session.php
@@ -2,7 +2,8 @@
 include_once("classes.php");
 
 $session_path = "/home/FALL2015/bt773/public_html/eHarbinger/sessions";
-$root_path = "/~bt773/eHarbinger/";
+// $root_path = "/~bt773/eHarbinger/";
+$root_path = "eHarbinger/";
 $index_path = $root_path . "index.php";
 
 
@@ -17,7 +18,7 @@ if (isset($_SESSION["user"])){
 		// echo $_SESSION["user"]->isLoggedIn();
 		// echo "Session verification successful<br />";
 		// echo $index_path;
-		if ($_SERVER["REQUEST_URI"] == $index_path || $_SERVER["REQUEST_URI"] == $root_path){
+		if ( substr($_SERVER["REQUEST_URI"], -10, 10) == $index_path || $_SERVER["REQUEST_URI"] == $root_path){
 			echo "yes";
 			Header("Location: players.php");
 			exit;
