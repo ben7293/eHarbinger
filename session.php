243ee0c7da152c1a5a02a4dbb68dce97c58bec12
Benson Tsai <ben7293@gmail.com>
2015-12-10 02:07:53 -0500

    Test

--- session.php
+++ session.php
@@ -1,10 +1,7 @@
 <?php
 include_once("classes.php");
 
-$session_path = "/home/FALL2015/bt773/public_html/eHarbinger/sessions";
-// $root_path = "/~bt773/eHarbinger/";
-$root_path = "eHarbinger/";
-$index_path = $root_path . "index.php";
+$index_path = "eHarbinger/index.php";
 
 
 // if (session_save_path() != $session_path) {session_save_path($session_path);}
@@ -18,9 +15,7 @@ if (isset($_SESSION["user"])){
 		// echo $_SESSION["user"]->isLoggedIn();
 		// echo "Session verification successful<br />";
 		// echo $index_path;
-		if ( substr($_SERVER["REQUEST_URI"], -11, 11) == $index_path || $_SERVER["REQUEST_URI"] == $root_path){
-			var_dump(substr($_SERVER["REQUEST_URI"], -11, 11));
-			die();
+		if ( substr($_SERVER["REQUEST_URI"], -10, 10) == $index_path || $_SERVER["REQUEST_URI"] == $root_path){
 			echo "yes";
 			Header("Location: players.php");
 			exit;
@@ -31,7 +26,7 @@ if (isset($_SESSION["user"])){
 else{
 	//If there is no session information
 	// echo "Session verification failed<br />";
-	if ($_SERVER["REQUEST_URI"] != $index_path){
+	if (substr($_SERVER["REQUEST_URI"], -10, 10) != $index_path){
 		Header("Location: index.php");
 		exit;
 	}
