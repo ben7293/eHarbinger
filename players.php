38d96d03f43947e5e2af3aa14791e8e9f2161997
Benson Tsai <ben7293@gmail.com>
2015-12-10 02:24:39 -0500

    Test

--- players.php
+++ players.php
@@ -33,7 +33,7 @@
 					echo "Username: $yourUserName<br>";
 					echo "Level: Expert<br>";
 					echo "Likes: " . $likeList["description"] . "<br>";
-					echo "Feedback: $rating['getrating']";
+					echo "Feedback: " . $rating["getrating"]";
 					echo "<form id='message' action='messages.php' method='get'>";
 						echo "<button onclick = 'send()'> Message </button>";
 						echo "<input type='hidden' name='user' value='$yourUserName'>";
