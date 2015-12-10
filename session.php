3206b29c8ac1790b66971fc2831470d46eef7efa
Benson Tsai <ben7293@gmail.com>
2015-12-10 02:05:15 -0500

    Test

--- session.php
+++ session.php
@@ -1,9 +1,13 @@
 <?php
 include_once("classes.php");
 
+$session_path = "/home/FALL2015/bt773/public_html/eHarbinger/sessions";
+// $root_path = "/~bt773/eHarbinger/";
 $root_path = "eHarbinger/";
 $index_path = $root_path . "index.php";
 
+
+// if (session_save_path() != $session_path) {session_save_path($session_path);}
 session_start();
 
 
