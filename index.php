e9086dab575ecd88248eb316c80c5cd78e36e53f
Benson Tsai <Benson Tsai>
2015-12-12 18:17:37 -0500

    Converted index.html to index.php, Megham pls add style to errmsg

--- index.php
+++ index.php
@@ -1,68 +1,82 @@
 <!DOCTYPE HTML>
+<!--
+	Prism by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
 <html>
 	<head>
-		<meta charset="utf-8"> 
-        <title>eHarbinger</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-		<link rel="shortcut icon" type="image/png" href="/favicon.png"/>
-        <script type="text/javascript" src = "design.js"></script>
-		<?php
-			if (!isset($_GET["err"])){
-//				include_once("session.php");
-			}
-		?>
+		<title> eHarbginer</title>
+		<meta charset="utf-8" />
+		<link rel= "stylesheet" href="css/main.css" />
 	</head>
 	<body>
-		<header>
-			<img src="resource/logo" height=100px></img>
-			Welcome! Please Fill In Your Information:
-			<br>
-		</header>
-		<div id = "col-1" class = "twoColumnContainer">
-			<form id="login" action="userauth.php" method="post">
-				<legend class = "heading"> Log In: </legend> <br>
-				<label for = "user"> Username: </label>
-				<br>
-				<input type = "text" name = "user"> <br>
-				<label for = "pxwd"> Password: </label>
-				<br>
-				<input type = "password" name = "pxwd"> <br>
-				<input type="hidden" name="type" value="login">
-				<?php
-					if ($_GET["err"] == 1){
-						echo "<font color=\"red\">Incorrect username and/or password.</font>";
-					}
-				?>				
-				<br><br>
-				<button> Submit </button>
-			</form>
-		</div>
-		<div id = "col-2" class = "twoColumnContainer">
-			<form id="signup" action="createacct.php" method="post">
-				<legend class = "heading"> Sign Up: </legend>
-				<br>
-				<label for = "Name"> Userame: </label>
-				<br>
-				<input type = "text" name = "user"> <br>
-				<label for = "Email"> Email: </label>
-				<br>
-				<input type = "email" name = "email"> <br>
-				<label for = "Password"> Password: </label>
-				<br>
-				<input type = "password" name = "pxwd"> <br>
-				<br>
-
-				<?php
-					if ($_GET["err"] == 2){
-						echo "<font color=\"red\">Username already exists.</font>";
-					}
-					elseif ($_GET["err"] == 3){
-						echo "<font color=\"red\">Your username and/or password is invalid.</font>";
-					}
-				?>								
-				<br><br>
-				<button onclick = "linkInfo()"> Submit </button>
-			</form>
-		</div>
+		<section id="banner">
+			<div class="inner split">
+				<section>
+					<h2>Welcome to eHarbinger!!</h2>
+				</section>
+				<section>
+					<p>Log In or Sign up for our wonderful "matchmaking" company!</p>
+				</section>
+			</div>
+		</section>
+		<section id="one" class="wrapper">
+			<div class="inner split">
+				<section>
+					<h2>Why should you join us?</h2>
+					<p>eHarbinger was created with the gamer in mind. We've developed a safe environment for gamers to be able to find others to play with. Our algorithm uses each person's game preferences and personality to match them with other gamers with the same qualities.</p>
+				</section>
+				<section>
+					<img class = "image-left" src = "images/logo.jpg" />
+				</section>
+			</div>
+		</section>
+			<section class="wrapper">
+				<div class="inner split">
+					<section class="errorMsg">
+						<!-- Megham pls-->
+						<?php
+							if ($_GET["err"] == 1){
+								echo "Incorrect username and/or password.";
+							}
+							elseif ($_GET["err"] == 2){
+								echo "Username already exists.";
+							}
+							elseif ($_GET["err"] == 3){
+								echo "Your username and/or password is invalid.";
+							}						
+						?>						
+					</section>
+					<section>
+						<h2>Log In</h2>
+						<form action="userauth.php" method="POST">
+							<div>
+								<input type = "text" name="username" placeholder = "Username">
+								<br>
+								<input type = "text" name="password" placeholder = "Password">
+								<br>
+ 							</div>
+							<input type="hidden" name="type" value="login">
+							<input value="Log In" type = "submit">
+ 						</form>
+					</section>
+					<section>
+						<h2>Sign Up</h2>
+						<form action = "createacct.php" method="POST">
+							<div>
+								<input type = "text" name = "name" placeholder = "Full Name">
+								<br>
+								<input type = "email" name = "email" placeholder = "Email">
+								<br>
+								<input type = "text" name = "password" placeholder = "Password">
+								<br>
+							</div>
+							<!-- Need to put with back end -->
+							<input value = "Sign Up" type = "submit">
+						</form>
+					</section>
+				</div>
+			</section>
 	</body>
 </html>
