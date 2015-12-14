9e6bac0055a20ef24ccb2d7fe6a5e22e934aaf4e
Brian Marks <bm1549@pdc-amd01.poly.edu>
2015-12-14 13:15:57 -0500

    no longer needed

--- header.php
+++
@@ -1,8 +0,0 @@
-<div id = "col-1">
-	<img src="images/logo.jpg" height=150px></img>
-	<form action="userauth.php" method="post">
-		<button type="submit"> Log Out </button>
-		<input type="hidden" name="type" value="logout">
-	</form>
-	<?php include('footer.html'); ?>
-</div>
