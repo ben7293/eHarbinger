99bff7978b435d90cab963a7da1a8907f79b51f0
Benson Tsai <Benson Tsai>
2015-12-14 01:33:30 -0500

    Test

--- updateMatches.cpp
+++ updateMatches.cpp
@@ -45,8 +45,7 @@ int main(){
 	
 	result myExpectation = conn.exec(myExpQuery);
 	result yourAnswer = conn.exec(yourAnsQuery);
-	int index = yourAnswer[0]["answerself"];
-	if ( myExpectation[0]["answerother"][ index ] == 1){
+	if ( myExpectation[0]["answerother"][ yourAnswer[0]["answerself"].as<int> ].as<int> == 1){
 		// If your answer is in my expectations
 		cout << "It's a match!\n";
 		// score += myExpectation[0]["importance"];
