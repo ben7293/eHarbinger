d968619d49ff0079735716adbd2ee71b22efa7d9
Benson Tsai <Benson Tsai>
2015-12-14 02:59:25 -0500

    Test

--- updateMatches.cpp
+++ updateMatches.cpp
@@ -16,8 +16,8 @@ bool isInList(const int target, result list){
 
 bool matchOneQuestion(work& conn, const int questionID, const string& myUserName, const string& yourUserName){
 	// Pull answers for each user
-	string myExpQuery = "select answerother, importance from users_answer_questions where questionID=" + questionID + "and username='" + string(myUserName) + "';";
-	string yourAnsQuery = "select answerself from users_answer_questions where questionID=" + questionID + "and username='" + string(yourUserName) + "';";
+	string myExpQuery = "select answerother, importance from users_answer_questions where questionID=" + (string)questionID + "and username='" + string(myUserName) + "';";
+	string yourAnsQuery = "select answerself from users_answer_questions where questionID=" + (string)questionID + "and username='" + string(yourUserName) + "';";
 	
 	result myExpectation = conn.exec(myExpQuery);
 	result yourAnswer = conn.exec(yourAnsQuery);
