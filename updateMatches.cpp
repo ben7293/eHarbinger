34130332eee9e7ff65f1989c42b1f13caf00ab4f
Benson Tsai <Benson Tsai>
2015-12-14 01:25:02 -0500

    Test

--- updateMatches.cpp
+++ updateMatches.cpp
@@ -5,33 +5,33 @@ using namespace std;
 using namespace pqxx;
 
 
-void matchOneQuestion(work& conn, const int questionID, const string& myUsername, const string& yourUserName){
-	// Pull answers for each user
-	result myExpectation = conn.exec("select answerother, importance from users_answer_questions;");
-	result yourAnswer = conn.exec("select answerself from users_answer_questions;");
-	if ( myExpectation[0]["answerother"][ yourAnswer[0]["answerself"] ] == 1){
-		// If your answer is in my expectations
-		score += myExpectation[0]["importance"];
-	}
-}
+// void matchOneQuestion(work& conn, const int questionID, const string& myUsername, const string& yourUserName){
+	// // Pull answers for each user
+	// result myExpectation = conn.exec("select answerother, importance from users_answer_questions;");
+	// result yourAnswer = conn.exec("select answerself from users_answer_questions;");
+	// if ( myExpectation[0]["answerother"][ yourAnswer[0]["answerself"] ] == 1){
+		// // If your answer is in my expectations
+		// score += myExpectation[0]["importance"];
+	// }
+// }
 
-void matchOneUserWithOthers(work& conn, const string& myUserName){
-	// First, grab a list of all users...
-	result userList = conn.exec("select username from users;");
-	for (int i=0; i < userList.size(); ++i){
-		// For each user
-		if (myUserName != userList[i]["username"]){
-			// We don't want to self-match
-			matchEachQuestions();
-		}
-	}
-	int totalscore = 0;
+// void matchOneUserWithOthers(work& conn, const string& myUserName){
+	// // First, grab a list of all users...
+	// result userList = conn.exec("select username from users;");
+	// for (int i=0; i < userList.size(); ++i){
+		// // For each user
+		// if (myUserName != userList[i]["username"]){
+			// // We don't want to self-match
+			// matchEachQuestions();
+		// }
+	// }
+	// int totalscore = 0;
 
-}
+// }
 
-void matchAllUsers(){
+// void matchAllUsers(){
 	
-}
+// }
 
 int main(){
 	// Connect to database
