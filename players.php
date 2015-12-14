6c88d4fffeca9ea9606af6d7b9acb19e4e994f4f
Benson Tsai <Benson Tsai>
2015-12-14 04:29:22 -0500

    Test

--- players.php
+++ players.php
@@ -11,6 +11,8 @@
 		// If no matches, find some for the poor user
 		echo "running match";
 		// system("./matchusers.exe");
+		shell_exec("ls", $a);
+		var_dump($a);
 		shell_exec("./matchusers.o $myUserName");
 		header("Refresh:0");
 	}
