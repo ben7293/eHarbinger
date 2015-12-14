a4c34709526ab3a4ffb0db0c6619bb0a4d52ba34
Benson Tsai <Benson Tsai>
2015-12-14 02:03:18 -0500

    Test

--- updateMatches.cpp
+++ updateMatches.cpp
@@ -50,7 +50,7 @@ int main(){
 	int index = yourAnswer[0]["answerself"].as<int>();
 	int theAns = answerOther[ index-1 ];
 	cout << "index = " << index-1 << endl;
-	cout << answerOther[ index-1 ];
+	cout << "theAns = " << answerOther[ index-1 ] << endl;
 	if ( theAns == 1){
 		// If your answer is in my expectations
 		cout << "It's a match!\n";
