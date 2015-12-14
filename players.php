dd7f1af5d10521294fcc8c0e2c38dec645e08d34
Benson Tsai <ben7293@gmail.com>
2015-12-13 21:51:50 -0500

    Added rate user functionality; converted new player.html to php

--- players.php
+++ players.php
@@ -3,15 +3,29 @@
 	<head>
 		<meta charset="utf-8"> 
         <title>eHarbinger</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="css/meg.css">
         <script type="text/javascript" src = "design.js"></script>
 		<?php
 			include_once("session.php");
-		?>
+			session_start();
+		?>		
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<?php include_once("header.php"); ?>
-		<div id = "col-2"  onload = "playerInfo()">
+	<section id = 'banner'>
+		<!-- Benson, need to change number of messages given -->
+		<div class = 'inner split'>
+			<section><h2> You Have This Many Matches: (2) </h2></section>
+			<section><button onclick = "logout()"> Log Out </button></section>
+		</div>
+	</section>
+		<div class = "wrapper">
+		<!-- Need to be able to upload matches of different players -->
 			<?php 
 			// First, fetch matches...
 			$myUserName = $_SESSION["user"]->getName();
@@ -24,29 +38,65 @@
 				// Fetch info of the other user
 				$yourUserName = $matchInfo['username'];
 				$yourProfile = $_SESSION["user"]->query("select * from getprofile('$yourUserName');", "array");
-				$likeList = trim($yourProfile["description"], ", ");
+				$description = trim($yourProfile["description"], ", ");
 				$rating = $_SESSION["user"]->query("select getrating('$yourUserName');", "array");
-				echo "<div id = 'avatar'>";
-				echo "</div>";
-					// echo "<img src = 'resource/avatar/$yourUserName' height='50px'>";
-					echo "<img src = 'resource/avatar/avatar' height='50px'>";
-				echo "<div>";
+				echo "<div class = 'inner split'  onload = "playerInfo()">";
+					echo "<section>";
+						echo "<img src = "images/plainPic.gif">";
+					echo "</section>";
+					echo "<section>";
+				echo "<section>";
 					echo "Username: <a href='profile.php?user=$yourUserName'>$yourUserName</a><br>";
 					echo "Level: Expert<br>";
-					echo "Likes: " . $likeList . "<br>";
+					echo "About the gamer: " . $description . "<br>";
 					echo "Feedback: " . $rating["getrating"];
+					//echo "<a href='#' class='btn btn-default'><span class='glyphicon glyphicon-heart'></span> Good Match</a>";
+					echo "<form id='likeMatch' action='rateuser.php' method='post'>";
+						echo "<input type='hidden' name='rating' value='1'>";
+						echo "<button>Good match</button>";
+					echo "</form>";
+					echo "<form id='unlikeMatch' action='rateuser.php' method='post'>";
+						echo "<input type='hidden' name='rateduser' value='$yourUserName'>";
+						echo "<input type='hidden' name='rating' value='-1'>";
+						echo "<button>I didn\'t like my match</button>";
+					echo "</form>";
+					echo "<br>";
+					//echo "<a href='#' class='btn btn-default'><span class='glyphicon glyphicon-trash'></span> I didn't like my match</a>";
 					echo "<form id='message' action='messages.php' method='get'>";
+						echo "<input type='hidden' name='rateduser' value='$yourUserName'>";
 						echo "<button onclick = 'send()'> Message </button>";
 						echo "<input type='hidden' name='user' value='$yourUserName'>";
 					echo "</form>";
-				echo "</div>";
+				echo "</section>";
 				
 				echo "<br>";
 
 			}
 
 			
-			?>
+			?>				
+		<div class = 'inner split'  onload = "playerInfo()">
+			<section> 
+				<img src = "images/plainPic.gif"> 
+			</section>
+			<section>
+				Username: champion<br>
+				Level: Expert<br>
+				Played With: 7 Players<br>
+				Likes: PC, MMO <br>
+				Feedback level
+				<br>
+
+				<a href="#" class="btn btn-default"><span class="glyphicon glyphicon-heart"></span> Good Match</a>
+				<br>
+				<a href="#" class="btn btn-default"><span class="glyphicon glyphicon-trash"></span> I didn't like my match</a>
+				<br>
+				<br>
+				<button onclick = "send()"> Message </button> 
+			</section>
+			<br>
 		</div>
+		</div>
+		<footer> <a href = "forum.html"> General Forum | </a> <a href = "messages.php"> Messages | </a> <a href = "contactUs.html"> Meet the Team! </a> </footer>
 	</body>
 </html>
---
+++ rateuser.php
@@ -0,0 +1,13 @@
+<?php
+include_once("classes.php");
+include_once("session.php");
+session_start();
+
+if (isset($_POST["rating"){
+	$me = $_SESSION["user"]->getName();
+	$you = $_POST["rateduser"];
+	$_SESSION["user"]->query("select rateuser($me, $you, $rating)","boolean")
+}
+header("Location: players.php");
+
+?>
