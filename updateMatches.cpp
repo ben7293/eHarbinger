3671058e6ad88a0649ca2dc702be4ecf60434d6b
Benson Tsai <Benson Tsai>
2015-12-14 03:24:59 -0500

    Test

--- updateMatches.cpp
+++ updateMatches.cpp
@@ -63,7 +63,7 @@ int matchOneUserWithOthers(work& conn, const string& myUserName){
 				}
 			}
 			
-			long matchRate = (totalScore / totalPossibleScore)*100;
+			long matchRate = (totalScore / totalPossibleScore)*10000;
 			int intMatchRate = int(matchRate);
 			cout << "Final score is " << totalScore << ", highest possible score is " << totalPossibleScore << ", your match rate is " << intMatchRate << endl;
 		}
