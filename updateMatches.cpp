f030d080ef870564181703a64006266dfb07d525
Benson Tsai <Benson Tsai>
2015-12-14 01:28:07 -0500

    Test

--- updateMatches.cpp
+++ updateMatches.cpp
@@ -40,8 +40,8 @@ int main(){
 	int questionID = 2;
 	string myUserName = "ben7293";
 	string yourUserName = "brian";
-	string myExpQuery = "select answerother, importance from users_answer_questions where questionID=" + questionID + "and username=" + myUserName + ";";
-	string yourAnsQuery = "select answerself from users_answer_questions where questionID=" + questionID + "and username=" + yourUserName + ";";
+	string myExpQuery = "select answerother, importance from users_answer_questions where questionID=" + string(questionID) + "and username=" + string(myUserName) + ";";
+	string yourAnsQuery = "select answerself from users_answer_questions where questionID=" + string(questionID) + "and username=" + string(yourUserName) + ";";
 	
 	result myExpectation = conn.exec(myExpQuery);
 	result yourAnswer = conn.exec(yourAnsQuery);
