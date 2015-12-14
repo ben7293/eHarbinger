000c3e1667914cf8249110b4d3f0b1d5acd445b6
Benson Tsai <Benson Tsai>
2015-12-13 23:16:29 -0500

    Test

--- createprofile.php
+++ createprofile.php
@@ -50,7 +50,7 @@ if (isset($_POST["pub_prof"])){
 	// Update profile
 	// $status = $_SESSION["user"]->upProf($username, $name, $location, $lang, $prefCsv);
 	$status = $_SESSION["user"]->upProf($username, $name, $location, $language, $description);
-	header("Location: profile.php");
+	header("Location: games.php");
 
 }
 
